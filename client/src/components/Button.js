43f604b8e39de0ca0d0196ee298e6e95c1317472
Will brown <wtbrown10@gmail.com>
2020-09-10 22:16:09 -0400

    added button and styled it

---
+++ client/src/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Button(props) {
+
+    console.log(props);
+
+    return (
+        <button
+
+        style={{...buttonStyle,...props.style}}
+        onClick={props.onClick}
+        
+        >
+        
+        {props.text}
+
+        </button>
+    )
+}
+
+const buttonStyle = {
+
+    padding: 10,
+    borderRadius: 5,
+    fontSize: 'large',
+    backgroundColor: 'lightblue',
+    color: 'black'
+}
